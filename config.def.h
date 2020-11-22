b690875e4744329f26b49d5a8cd229f47e4f83ec
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-22 04:04:49 +0100

    Changes terminal font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:size=20:antialias=true:autohint=true";
+static char *font = "Iosevka:size=13:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
